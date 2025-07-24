67be6dba1b7d4a5dea6e93ddf6dc004f83b82a6a
ramoliyaYug <mastermind57369@gmail.com>
2025-07-24 13:11:29 +0530

    fixed all issues related to tabs

--- script.js
+++ script.js
@@ -422,8 +422,8 @@ function updateTabsDisplay() {
         };
 
         tab.innerHTML = `
-            <span>${fileDisplayNames[fileName] || fileName + '.kt'}</span>
-            <span class="close-tab">
+            <span onclick="switchToTab('${fileName}')">${fileDisplayNames[fileName] || fileName + '.kt'}</span>
+            <span class="close-tab" onclick="closeFileTab('${fileName}')">
                 <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <line x1="18" y1="6" x2="6" y2="18"/>
                     <line x1="6" y1="6" x2="18" y2="18"/>
